c42fe93038b076c0aa2962e6a34052b4e2f549c9
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2022-06-15 23:34:47 -0400

    Reformatted comments

--- working_with_lists/p13.ml
+++ working_with_lists/p13.ml
@@ -1,6 +1,8 @@
 (* Run-length encoding of a list (direct solution) *)
 
-(* Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists containing the duplicates, as in problem "Pack consecutive duplicates of list elements into sublists", but only count them. As in problem "Modified run-length encoding", simplify the result list by replacing the singleton lists (1 X) by X. *)
+(* Implement the so-called run-length encoding data compression method directly. I.e. don't explicitly create the sublists
+   containing the duplicates, as in problem "Pack consecutive duplicates of list  elements into sublists", but only count them.
+   As in problem "Modified run-length encoding", simplify the result list by replacing the singleton lists (1 X) by X. *)
 
 (* # encode ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"];; *)
 (* - : string rle list = *)
