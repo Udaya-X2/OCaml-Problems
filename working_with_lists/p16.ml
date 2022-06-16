3e56ed7e3664d3c1555955c6878e11e882a0a6ce
Udaya-X2 <41876812+Udaya-X2@users.noreply.github.com>
2022-06-15 21:17:36 -0400

    Completed problem 16

---
+++ working_with_lists/p16.ml
@@ -0,0 +1,19 @@
+(* Drop every N'th element from a list *)
+
+(* Drop every N'th element from a list. *)
+
+(* # drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3;; *)
+(* - : string list = ["a"; "b"; "d"; "e"; "g"; "h"; "j"] *)
+
+(* My solution *)
+let drop (l : 'a list) (n : int) : 'a list =
+  let aux (k : int) = function
+  | hd :: tl -> if k = n then aux 
+  | [] -> 
+
+(* Website solution *)
+let drop list n =
+  let rec aux i = function
+    | [] -> []
+    | h :: t -> if i = n then aux 1 t else h :: aux (i + 1) t  in
+  aux 1 list
